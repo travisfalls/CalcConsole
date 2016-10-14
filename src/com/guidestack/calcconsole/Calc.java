eb341b9b60d9971423f71c8b29c61993d1bcef33
Travis Falls <Travis@Kadys-MacBook-Pro.local>
2016-10-14 08:46:49 -0400

    first commit

---
+++ src/com/guidestack/calcconsole/Calc.java
@@ -0,0 +1,22 @@
+package com.guidestack.calcconsole;
+
+public class Calc {
+
+	public double add(double a, double b) {
+		return a+b;
+	}
+	
+	public double subtract(double a, double b) {
+		return a-b;
+	}	
+		
+	public double multiply(double a, double b) {
+		return a*b;
+	}
+	
+	public double divide(double a, double b) {
+		return a/b;
+	}
+		
+	
+}
---
+++ src/com/guidestack/calcconsole/Main.java
@@ -0,0 +1,23 @@
+package com.guidestack.calcconsole;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Calc calc = new Calc();
+		double a = 0;
+		double b = 0;
+		String sign = "";
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		a = Double.
+		
+		
+	}
+
+}
